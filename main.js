fae36440d146caaa06f63526fd7e898d5c9d0d12
Mohamed Shadab <shadabdragon@gmail.com>
2018-02-09 15:45:23 +0530

    Completed alot of the UI elements

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {
     app,
-    BrowserWindow
+    BrowserWindow,
+    Menu
 } = require('electron');
 const path = require('path');
 const url = require('url');
@@ -9,8 +10,8 @@ let win;
 
 function windowCreate() {
     win = new BrowserWindow({
-        width: 600,
-        height: 500,
+        width: 800,
+        height: 600,
         icon: __dirname
     });
 
@@ -19,7 +20,66 @@ function windowCreate() {
         protocol: 'file',
         slashes: true
     }));
+
+    //Build menu
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    //Insert menu
+    Menu.setApplicationMenu(mainMenu);
 }
+
+//      Menu Template
+
+const mainMenuTemplate = [{
+        label: 'File',
+        submenu: [{
+            // Functionality required.
+                label: 'Reset'
+            },
+            {
+                label: 'Exit',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label: 'Help',
+        submenu: [{
+            label: 'Instructions',
+            click() {
+                const modalPath = path.join(__dirname, 'instruction.html')
+                let win = new BrowserWindow({
+                    frame: false,
+                    width: 650,
+                    height: 500
+                })
+                win.on('close', function () {
+                    win = null
+                })
+                win.loadURL(modalPath)
+                win.show()
+            }
+        }]
+    },
+    //Temporary for Devlopment purposes
+    {
+        label: 'DevTools',
+        accelerator: (function () {
+          if (process.platform === 'darwin') {
+            return 'Alt+Command+I'
+          } else {
+            return 'Ctrl+Shift+I'
+          }
+        })(),
+        click: function (item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.toggleDevTools()
+          }
+        }
+      }
+]
 
 app.on('ready', windowCreate);
 
